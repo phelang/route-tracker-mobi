7752ebd42bc222cb8ccf9b2f6f9892488bd9ba49
phelang.qhu@hotmail.com <phelang.qhu@gmail.com>
2021-03-18 14:10:20 +0200

    Add error handling for location permission service

--- src/components/Map.js
+++ src/components/Map.js
@@ -6,46 +6,45 @@ import pin from '../../assets/pin.png'
 
 const Map = ({ initialCoords, currentLocation, locations, recording }) => {
   return (
-    <>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          ...initialCoords.coords,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01,
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        ...initialCoords.coords,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
+    >
+      {currentLocation ? (
+        <Circle
+          center={currentLocation.coords}
+          radius={25}
+          strokeColor='rgba(158, 159, 255, 1.0)'
+          fillColor='rgba(158, 159, 255,0.3)'
+        />
+      ) : null}
+
+      <Marker
+        coordinate={{
+          longitude: initialCoords.coords['longitude'],
+          latitude: initialCoords.coords['latitude'],
         }}
-      >
-        {currentLocation ? (
-          <Circle
-            center={currentLocation.coords}
-            radius={25}
-            strokeColor='rgba(158, 159, 255, 1.0)'
-            fillColor='rgba(158, 159, 255,0.3)'
-          />
-        ) : null}
+        image={pin}
+      />
 
-        <Marker
-          coordinate={{
-            longitude: initialCoords.coords['longitude'],
-            latitude: initialCoords.coords['latitude'],
-          }}
-          image={pin}
-        />
-
-        <Polyline
-          coordinates={locations.map((loc) => loc.coords)}
-          strokeWidth={4}
-          strokeColor='rgb(255,223,0)' //''
-          geodesic={true}
-        />
-      </MapView>
-    </>
+      <Polyline
+        coordinates={locations.map((loc) => loc.coords)}
+        strokeWidth={4}
+        strokeColor='rgb(255,223,0)' //''
+        geodesic={true}
+      />
+    </MapView>
   )
 }
 
 const styles = StyleSheet.create({
   map: {
-    // flex: 1,
+    marginTop: 0,
+    paddingTop: 0,
     height: '80%',
     shadowOpacity: 0.3,
   },
